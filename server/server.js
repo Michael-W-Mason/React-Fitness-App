2d1dc7c2cf5cbc89de28f613123f46acce0c45fb
Michael Mason <mwmason1998@gmail.com>
2022-06-27 17:07:41 -0400

    test

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+// app.use(cors({credentials: true, origin: "mni"}));
+app.options('*', cors({credentials: true}))
 
 require("./config/mongoose.config");
 app.use(express.json(), express.urlencoded({ extended: true }));
