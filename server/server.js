396d98fa26beb80cc693c8ca8462a375d6c38a91
Michael Mason <mwmason1998@gmail.com>
2022-06-28 14:00:16 -0400

    allows app to be proxied

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "https://michaelmason.dev" }));
+app.set("trust proxy", true);
 
 require("./config/mongoose.config");
 app.use(express.json(), express.urlencoded({ extended: true, credentials: true }));
