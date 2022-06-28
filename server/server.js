97be11b1ca93a5125b60e1426c1747f263e642f6
Michael Mason <mwmason1998@gmail.com>
2022-06-28 13:11:01 -0400

    cors issue

--- server/server.js
+++ server/server.js
@@ -6,11 +6,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:8000" }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
+app.use(cors({ credentials: true, origin: "https://michaelmason.dev" }));
 
 require("./config/mongoose.config");
 app.use(express.json(), express.urlencoded({ extended: true }));
